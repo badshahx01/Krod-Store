edd0a9d1f3c94f2ed7cd5ac7d52bb0a3e9312fb0
badshahx01 <krishh111107@gmail.com>
2026-03-16 05:01:32 +0530

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'krod-store-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/products.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => {
+        return caches.match('/index.html');
+      })
+  );
+});
